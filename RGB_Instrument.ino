38b15a0e5761ca11ebf85f7bc701b842a2ce823a
Linsheng Pan <141114823+ray-uncoding@users.noreply.github.com>
2024-01-08 23:52:57 +0800

    加入開關機燈

--- RGB_Instrument.ino
+++ RGB_Instrument.ino
@@ -2,19 +2,23 @@
 #include <ESPAsyncWebSrv.h>
 #include <Adafruit_NeoPixel.h>
 
-#define NUM_UNITS 3                                                     // 樂器單元數量
-#define NUM_LEDS_PER_UNIT 13                                             // 每個單元的LED數量
-#define NUM_LEDS_TOTAL (NUM_UNITS * NUM_LEDS_PER_UNIT)                  // 總LED數量
-#define LED_PIN 11                                                      // 連接第一個LED的腳位
-Adafruit_NeoPixel leds(NUM_LEDS_TOTAL, LED_PIN, NEO_GRB + NEO_KHZ800);  //  定義ws2812燈條
+#define POWER_ONOFF_PIN 6
+#define NUM_UNITS 3           // 樂器單元數量
+#define NUM_LEDS_PER_UNIT 13  // 每個單元的LED數量
+#define NUM_POWER_ONOFF_LED 18
+#define NUM_LEDS_TOTAL (NUM_UNITS * NUM_LEDS_PER_UNIT + NUM_POWER_ONOFF_LED)  // 總LED數量
+#define LED_PIN 11                                                            // 連接第一個LED的腳位
+Adafruit_NeoPixel leds(NUM_LEDS_TOTAL, LED_PIN, NEO_GRB + NEO_KHZ800);        //  定義ws2812燈條
 
 float client1_RGB[3] = { 100.00, 144.00, 232.00 };  //調整樂器單元顏色
 float client2_RGB[3] = { 72.00, 193.00, 172.00 };
 float client3_RGB[3] = { 255.00, 71.00, 34.00 };
+float powerONOFF_RGB[3] = { 255.00, 255.00, 255.00 };
 
 float client1_Bright = 0.10;  //調整樂器單元亮度
 float client2_Bright = 0.10;
 float client3_Bright = 0.10;
+float powerONOFF_Bright = 0.50;
 float brightIntervel = 0.04;
 
 int client1_chang = 1;  //調整樂器單元亮度變化
@@ -32,11 +36,11 @@ void setup() {
   leds.begin();  //led初始化
   leds.show();
   /*-----------*/
+  pinMode(POWER_ONOFF_PIN, INPUT_PULLUP);
 }
 void loop() {
-  deloperSerialCmdMode();
-  
   ONorOFFAnimate();
+  deloperSerialCmdMode();
   /*------on-------*/
   if (workState) {
     allBrightToTen();
@@ -90,6 +94,7 @@ void allClientVerToZero() {
   client1_chang = 0;
   client2_chang = 0;
   client3_chang = 0;
+  powerONOFF_Bright = 0;
 }
 void allBrightToTen() {
   brightToTen(client1_Bright, client1_chang);
@@ -102,12 +107,15 @@ void allBrightToZero() {
   brightToZero(client3_Bright, client3_chang);
 }
 void ONorOFFAnimate() {
+  bool term = digitalRead(POWER_ONOFF_PIN);
+  workState = term;
   if (workState == true && last_workState == false) {  //開機動畫
     allClientVerToZero();
     for (int i = 0; i < 100; i++) {
       client1_Bright += 0.001;
       client2_Bright += 0.001;
       client3_Bright += 0.001;
+      powerONOFF_Bright += 0.005;
       refreshBright();
       delay(40);
     }
@@ -122,7 +130,10 @@ void ONorOFFAnimate() {
       if (client3_Bright > 0) {
         client3_Bright -= 0.01;
       }
-      if (client1_Bright <= 0 && client1_Bright <= 0 && client1_Bright <= 0) {
+      if (powerONOFF_Bright > 0) {
+        powerONOFF_Bright -= 0.05;
+      }
+      if (client1_Bright <= 0 || client2_Bright <= 0 || client3_Bright <= 0 || powerONOFF_Bright <= 0) {
         allClientVerToZero();
         i = 100;
       }
@@ -140,6 +151,9 @@ void refreshBright() {
   }
   for (int i = 2 * NUM_LEDS_PER_UNIT; i < 3 * NUM_LEDS_PER_UNIT; i++) {
     leds.setPixelColor(i, client3_Bright * client3_RGB[0], client3_Bright * client3_RGB[1], client3_Bright * client3_RGB[2]);
+  }
+  for (int i = 3 * NUM_LEDS_PER_UNIT; i < NUM_LEDS_TOTAL; i++) {
+    leds.setPixelColor(i, powerONOFF_Bright * powerONOFF_RGB[0], powerONOFF_Bright * powerONOFF_RGB[1], powerONOFF_Bright * powerONOFF_RGB[2]);
   }
   leds.show();
 }
